81e338502f4dcbc7b6f2c93cdcbe4e1806b6dd38
vijeth11 <vijeth11@yahoo.co.in>
2022-02-07 14:40:34 -0500

    added changes UI zoom and pan

--- nodejs-assignments/assignemnt5-angular-mongoose/frontend/boilerplate/src/app/app.component.ts
+++ nodejs-assignments/assignemnt5-angular-mongoose/frontend/boilerplate/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { MessageService } from './services/message.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TodoService } from './services/todo.service';
 import { Subscription } from 'rxjs';
-
+import createPanZoom from 'panzoom';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,8 +16,10 @@ export class AppComponent implements OnInit,OnDestroy{
     listId: number,
     listItem: string
   }[] = [];
+  public currentIndex:number =-1;
 
   private messageReadSub:Subscription|null = null;
+  private todoElements:HTMLElement[] = [];
 
   constructor(private todoService:TodoService, private messageService:MessageService){
     this.getTodoList();
@@ -49,6 +51,70 @@ export class AppComponent implements OnInit,OnDestroy{
   ngOnInit(){
     this.messageReadSub = this.messageService.message
     .subscribe((msg:string) => this.messages.push(msg));
+    let todoElement = (<HTMLElement>document.querySelector('#todo1'));
+    let todoElement2 = (<HTMLElement>document.querySelector('#todo2'));
+    let background = <HTMLElement>document.querySelector('#background');
+    this.todoElements = [todoElement,todoElement2];
+    //panzoom1.panzoom("#todo1");
+    //panzoom1.panzoom("#background");
+    let panBackgroundInstance = createPanZoom(background,{
+      beforeWheel: function(e) {
+        // allow wheel-zoom only if altKey is down. Otherwise - ignore
+        var shouldIgnore = !e.altKey;
+        return shouldIgnore;
+      },
+      beforeMouseDown: function(e) {
+        // allow mouse-down panning only if altKey is down. Otherwise - ignore
+        var shouldIgnore = !e.altKey;
+        return shouldIgnore;
+      }
+    });
+    let panInstance = createPanZoom(todoElement,{      
+      beforeWheel:(event:WheelEvent)=>{
+        return !this.wheelEffect(0);
+      },
+      beforeMouseDown:(event:MouseEvent)=>{
+        return !this.mouseEffect(0);
+      },
+    });
+    let panInstance2 = createPanZoom(todoElement2,{      
+      beforeWheel:(event:WheelEvent)=>{
+        return !this.wheelEffect(1);
+      },
+      beforeMouseDown:(event:MouseEvent)=>{
+        return !this.mouseEffect(1);
+      },
+    });
+   panInstance.on('pan', function(e) {
+      console.log('Fired when the `element` is being panned', e);
+    });
+    
+    panInstance.on('panend', function(e) {
+      console.log('Fired when pan ended', e);
+    });
+    
+    panInstance.on('zoom', (e) => {
+      console.log('Fired when `element` is zoomed', e);
+    });
+    
+    panInstance.on('zoomend', (e) => {
+      console.log('Fired when zoom animation ended', e);
+      this.currentIndex = -1;
+    });
+    
+    panInstance.on('transform', (e) =>{
+      // This event will be called along with events above.
+      console.log('Fired when any transformation has happened', e);
+      this.currentIndex = -1;
+    });
+  }
+
+  wheelEffect(index:number){    
+    return this.currentIndex == index;
+  }
+
+  mouseEffect(index:number){
+    return this.currentIndex == index;
   }
 
   ngOnDestroy(){
--- nodejs-assignments/assignemnt5-angular-mongoose/frontend/boilerplate/src/app/app.module.ts
+++ nodejs-assignments/assignemnt5-angular-mongoose/frontend/boilerplate/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoModule } from 'ngx-socket-io';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SocketIoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
